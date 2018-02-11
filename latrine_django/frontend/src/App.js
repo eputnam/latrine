065a94789d8cbc77e0d1e9f094788e99d28bab07
jastr945 <polina.jastr@gmail.com>
2018-02-10 18:17:56 -0800

    Added a cat gif

--- latrine_django/frontend/src/App.js
+++ latrine_django/frontend/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={require('./images/kitty.gif')} alt="kitty" />
       </div>
     );
   }
