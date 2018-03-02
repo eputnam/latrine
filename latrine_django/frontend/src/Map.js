18db849c8da58b30032e34deb9cbaf3c66dc78a0
jastr945 <polina.jastr@gmail.com>
2018-03-02 00:48:31 -0800

    Installed google-maps-react, modified the map, added bathrooms using RefugeRestrooms API

--- latrine_django/frontend/src/Map.js
+++ latrine_django/frontend/src/Map.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './Map.css';
 
-export default class Map extends React.Component {
+
+export class MapContainer extends Component {
 
   constructor() {
     super()
     this.state = {
-      restrooms: null,
+      restrooms: [],
     }
   }
 
   componentDidMount() {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 45.5221418, lng: -122.6776808},
-      zoom: 13,
-      mapTypeId: 'roadmap',
-    });
-
-    // Calling RefugeRestrooms API
-    axios.get('https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?lat=45.5221418&lng=-122.677680')
+    // Calling RefugeRestrooms.org API
+    axios.get('https://www.refugerestrooms.org:443/api/v1/restrooms/search.json?per_page=100&query=portland')
     .then((response) => {
       console.log(response.data);
-      this.setState({ restrooms: JSON.stringify(response.data) });
+      this.setState({ restrooms: response.data });
     })
     .catch((error) => {
       console.log(error);
@@ -31,7 +27,39 @@ export default class Map extends React.Component {
 
   render() {
     return (
-      <div id='map' />
+      <Map
+      google={this.props.google}
+      style={{width: '100%', height: '100%'}}
+      zoom={14}
+      initialCenter={{
+        lat: 45.5231,
+        lng: -122.6765
+      }}
+      >
+        {
+          this.state.restrooms.map((i, markerindex) => {
+            const google = window.google;
+            return (
+              <Marker
+                key={markerindex}
+                title={'RefugeRestrooms.org -' + i.street}
+                name={'RefugeRestrooms.org'}
+                position={{ lat: i.latitude, lng: i.longitude }}
+                icon={{
+                  url: require("./images/potty.png"),
+                  anchor: new google.maps.Point(32,32),
+                  scaledSize: new google.maps.Size(45,45)
+                }}
+              />
+            )
+          })
+        }
+      </Map>
     );
   }
 }
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyCxZwna2Pr9JMOm1mUZC1iSMjiWaegroHo'),
+  version: '3.31'
+})(MapContainer)
