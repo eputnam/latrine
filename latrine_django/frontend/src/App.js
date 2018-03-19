c976e1f01e4789c4c583b1caffd5b89b96e334ac
jastr945 <polina.jastr@gmail.com>
2018-03-18 21:38:48 -0700

    Added a filter for restrooms, resized the map

--- latrine_django/frontend/src/App.js
+++ latrine_django/frontend/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import MapContainer from './Map.js'
+import MapContainer from './Map.js';
+import Filters from './Filters.js';
 import './App.css';
 
 export default class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      restroomsChecked: true
+    }
+    this.toggleRestrooms = this.toggleRestrooms.bind(this);
+  }
+
+  toggleRestrooms() {
+    this.setState({
+      restroomsChecked: !this.state.restroomsChecked
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <MapContainer />
+        <Filters restroomsChecked={this.state.restroomsChecked} toggleRestrooms={this.toggleRestrooms} />
+        <MapContainer restroomsChecked={this.state.restroomsChecked} />
       </div>
     );
   }
