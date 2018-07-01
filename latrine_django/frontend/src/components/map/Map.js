e509c48f6dfd477a2bf68c52e9b3133c8f707f40
Jon <jon.walz.music@gmail.com>
2018-07-01 12:57:26 -0700

    Removed unused axios import

--- latrine_django/frontend/src/components/map/Map.js
+++ latrine_django/frontend/src/components/map/Map.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import './Map.css';
 const pottyImage = require('../../images/potty.png');
