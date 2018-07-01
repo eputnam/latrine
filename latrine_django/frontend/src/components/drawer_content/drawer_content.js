978cf4371f61a5119690c84de3cf3047e4f5caa1
Jon <jon.walz.music@gmail.com>
2018-07-01 12:43:36 -0700

    Removed console log from drawer component

--- latrine_django/frontend/src/components/drawer_content/drawer_content.js
+++ latrine_django/frontend/src/components/drawer_content/drawer_content.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class DrawerContent extends React.Component {
     render() {
-        console.log('restroom props: ', this.props.restrooms);
         return <div>Drawer</div>;
     }
 }
