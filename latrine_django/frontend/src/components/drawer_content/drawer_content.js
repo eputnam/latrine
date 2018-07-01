dba311f88449fefaa08ac00d53fac770d0194158
Jon <jon.walz.music@gmail.com>
2018-07-01 15:23:10 -0700

    Added info to sidebar content component

--- latrine_django/frontend/src/components/drawer_content/drawer_content.js
+++ latrine_django/frontend/src/components/drawer_content/drawer_content.js
@@ -3,8 +3,21 @@ import React from 'react';
 export default class DrawerContent extends React.Component {
     render() {
         const {
-            selectedRestroomData: { name },
+            selectedRestroomData: { name, comment, street, city, country },
         } = this.props;
-        return <div>{name}</div>;
+
+        //
+        //Sidebar displays the Resources and Place information including photo, address, phone, comments, etc.
+        //
+
+        return (
+            <ul>
+                <li>{name}</li>
+                <li>{comment}</li>
+                <li>{street}</li>
+                <li>{city}</li>
+                <li>{country}</li>
+            </ul>
+        );
     }
 }
