97b7068c301ccadfffc87ff3458fc2268ddf958c
Jon <jon.walz.music@gmail.com>
2018-07-01 15:38:57 -0700

    Added styled-components

--- latrine_django/frontend/src/components/drawer_content/drawer_content.js
+++ latrine_django/frontend/src/components/drawer_content/drawer_content.js
@@ -1,23 +1,31 @@
 import React from 'react';
 
 export default class DrawerContent extends React.Component {
-    render() {
+    constructor() {
+        super();
+
+        this.makeListItems = this.makeListItems.bind(this);
+    }
+
+    makeListItems() {
         const {
             selectedRestroomData: { name, comment, street, city, country },
         } = this.props;
-
         //
         //Sidebar displays the Resources and Place information including photo, address, phone, comments, etc.
         //
-
         return (
-            <ul>
+            <React.Fragment>
                 <li>{name}</li>
                 <li>{comment}</li>
                 <li>{street}</li>
                 <li>{city}</li>
                 <li>{country}</li>
-            </ul>
+            </React.Fragment>
         );
+    }
+
+    render() {
+        return <ul>{this.makeListItems()}</ul>;
     }
 }
